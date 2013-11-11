c77cfc01c9f12b1fd7cb8168a6f156c288ef95dc
Tyler Van Hoomissen <linuxsable@gmail.com>
2013-11-10 16:09:17 -0800

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ReviewMyCode::Application.config.secret_key_base = '73085c5fe993c9e8ec196e8f9746863448732cd8471f18dc663e0e4a6ebe513cd929a3290d54a7186b4ea715d88102aba7f0b789ed3263a9c80dc97fbf81fe09'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ReviewMyCode::Application.config.session_store :cookie_store, key: '_review_my_code_session'
